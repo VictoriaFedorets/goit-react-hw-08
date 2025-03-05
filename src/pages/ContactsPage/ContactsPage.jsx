bfa8ad4c5e72823950960d65af6108596fb465c2
Victoria <vika96karnauhova@gmail.com>
2025-03-06 01:25:00 +0200

    changes

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,14 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import PageTitle from "../../components/PageTitle/PageTitle";
+import WrapperForm from "../../components/WrapperForm/WrapperForm";
 import ContactList from "../../components/ContactList/ContactList";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 
 import { fetchContacts } from "../../redux/contacts/operations";
 import { selectLoading, selectError } from "../../redux/contacts/selectors";
-
 
 export default function ContactsPage() {
   const dispatch = useDispatch();
@@ -20,13 +19,13 @@ export default function ContactsPage() {
   }, [dispatch]);
 
   return (
-    <>
-      <PageTitle>Your contacts</PageTitle>
+    <WrapperForm>
+      <p>Your contacts</p>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
       <div>{isLoading && !isError && "Loading contacts..."}</div>
       <ContactList />
-    </>
+    </WrapperForm>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Разделяем все зависимости из node_modules в отдельный чанк 'vendor'
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Увеличиваем лимит для предупреждений о размерах чанков (в КБ)
   },
 });
